6246acc9690f33933327f24d2953efa7ea43b32e
Sona Aseyednezhad <sona.aseyednejad@gmail.com>
2025-04-14 18:10:41 +0200

    modify the acceptor_plotting to plot electron acceptor and reduced electron acceptors and save the generated plots in resuts folder

--- scripts/visualize/CW_data_analysis_scripts/acceptors_compound_plotting.py
+++ scripts/visualize/CW_data_analysis_scripts/acceptors_compound_plotting.py
@@ -14,27 +14,36 @@ import matplotlib.pyplot as plt
 
 #%%
 # Contaminants to be plotted
-
-compounds = ["Mangaan (II)", "Ijzer (2+)", "sulfaat"]
+# Electron acceptors
+compounds = ["Zuurstof", "nitraat", "sulfaat"]
+# Reduced electron acceptors
+#compounds = ["sulfide (vrij)", "Mangaan (II)", "Ijzer (2+)"]
 
 # Rename the contaminants to be plotted (optional), put them in the same order as above.
-rename = ["Manganese (II)", "Iron (II)", "Sulfate"]
+# Rename electron acceptors
+rename = ["Oxygen", "Nitrate", "Solphate"]
+
+# Rename reduced electron acceptors
+#rename = ["sulfide (free)", "Manganese (II)", "Iron (II)"]
 
 # Locations to plot, using the names of location_dictionary.
 colors = ["#89003d", "#f0ab70", "#ffd220", "#66a697", "#004D8A"]
-plots = ["CW1_shallow", "CW2_shallow", "CW3_shallow"]
+plots = ["CW1_deep", "CW2_deep", "CW3_deep"]
 titles = ["CW1", "CW2", "CW3"]
 # Relative file path to the excel file with the data
 time_points = [0,1,2,3]
 
 normalize = True
 compensate_dilution = True
-ylimit_list = [(-0.5, 30), (-1, 100), (-0.1, 5)]
+ylimit_list = [(-0.5, 10), (-0.5, 30), (-0.1, 5)]
 
 df_list = []
 for time in time_points:
     file_path = f"../../../data/raw/CW_field_meassurements/Raw_data_lab/240120_Resultaten_ronde_T{time}.xlsx"
     df_list.append(tools.cleanup_compound(file_path))
+
+#print(df_list)
+
 
 #%% 
 l = 0
@@ -63,18 +72,62 @@ for i, compound in enumerate(compounds):
             
             dataframe = df_list[k]
             
-            plot_frame = dataframe.drop(columns = "unit", axis=0)
-            
+            plot_frame = dataframe.drop(columns = "unit", axis=0)  
+
             # Influent is the same for each wetland, thus divide by it to normalize data.
-            if normalize:
-                plot_frame = plot_frame.div(plot_frame["INF"], axis = 0)
+            # if normalize:
+            #     plot_frame = plot_frame.div(plot_frame["INF"], axis = 0)
             
+            # if compensate_dilution:
+            #     if not normalize:
+            #         comp_chlor = plot_frame.div(plot_frame["INF"], axis = 0)
+            #         plot_frame = plot_frame.div(comp_chlor.loc["chloride"])
+            #     else:
+            #         plot_frame = plot_frame.div(plot_frame.loc["chloride"])
+            
+            # --- STEP 1: Conditional Normalization (for plotting normalized results) ---
+
+            if normalize:
+                norm_frame = plot_frame.copy()
+
+                for cmpd in plot_frame.index:
+                    if cmpd != "Zuurstof":
+                        try:
+                            # Get the scalar INF value for this compound
+                            inf_value = plot_frame.at[cmpd, "INF"]
+                                
+                            # Normalize the full row
+                            norm_frame.loc[cmpd] = plot_frame.loc[cmpd] / inf_value
+
+                        except Exception as e:
+                            print(f"Skipping {cmpd} due to error: {e}")
+
+                plot_frame = norm_frame
+
+
+            # --- STEP 2: If chloride correction is requested ---
             if compensate_dilution:
+                
+                # If normalize is False, we still need to normalize (except Zuurstof) to apply chloride correction
                 if not normalize:
-                    comp_chlor = plot_frame.div(plot_frame["INF"], axis = 0)
-                    plot_frame = plot_frame.div(comp_chlor.loc["chloride"])
-                else:
-                    plot_frame = plot_frame.div(plot_frame.loc["chloride"])
+                    for cmpd in plot_frame.index:
+                        if cmpd != "Zuurstof":
+                            try:
+                                # Get the scalar INF value for this compound
+                                inf_value = plot_frame.at[cmpd, "INF"]
+                                    
+                                # Normalize the full row
+                                norm_frame.loc[cmpd] = plot_frame.loc[cmpd] / inf_value
+
+                            except Exception as e:
+                                print(f"Skipping {cmpd} due to error: {e}")
+
+                    plot_frame = norm_frame
+
+            #Now apply chloride-based dilution correction to all compounds
+            chloride_correction = plot_frame.loc["chloride"]
+            plot_frame = plot_frame.div(chloride_correction)
+
             
             # Select requested locations and compounds
             plot_frame = plot_frame[locations].loc[compound].T
