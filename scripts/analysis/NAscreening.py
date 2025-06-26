e2eb1cc92d2f9caae042f20998c0d271eca7c165
Sona Aseyednezhad <sona.aseyednejad@gmail.com>
2025-06-26 10:15:24 +0200

    modefy NAscreening.py according to  mibiscreen release v0.5.0

--- scripts/analysis/NAscreening.py
+++ scripts/analysis/NAscreening.py
@@ -1,29 +1,22 @@
-
-import mibiscreen.analysis.sample.screening_NA as na
-from mibiscreen.data.check_data import standardize
-from mibiscreen.data.load_data import load_csv
+import mibiscreen as mbs
 from IPython.display import display
 
-time_point = "4"
-file_path = f"../../data/preprocessed/na_screening/cw_T{time_point}_preprocessed.csv"
+file_path = "../../data/preprocessed/na_screening/cw_T4_preprocessed.csv"
+data_raw,units = mbs.load_csv(file_path, verbose = True)
 
-data_raw,units = load_csv(file_path, verbose = True)
+data,units = mbs.standardize(data_raw,reduce = True, verbose = True)
 
-data,units = standardize(data_raw,reduce = True, verbose = True)
+tot_redct = mbs.reductors(data, verbose = True, ea_group = "ONS")
 
-tot_redct = na.reductors(data, verbose = True, ea_group = "ONS")
+tot_oxi = mbs.oxidators(data, verbose = True, contaminant_group="BTEX")
 
-tot_oxi = na.oxidators(data, verbose = True, contaminant_group="BTEX")
+e_bal = mbs.electron_balance(data, verbose=True)
 
-e_bal = na.electron_balance(data, verbose=True)
+na_traffic = mbs.sample_NA_traffic(data,verbose = True)
 
-na_traffic = na.NA_traffic(data,verbose = True)
+tot_cont = mbs.total_contaminant_concentration(data, verbose = True, contaminant_group="BTEX")
 
-tot_cont = na.total_contaminant_concentration(data, verbose = True, contaminant_group="BTEX")
+na_interventation = mbs.thresholds_for_intervention(data, verbose=True, contaminant_group="BTEX")
 
-na_interventation = na.thresholds_for_intervention(data, verbose=True, contaminant_group="BTEX")
-
-data_na = na.screening_NA(data)
-#display(data_na)
-
-data_na.to_excel(f"../../results/na_screening_result_T{time_point}.xlsx", index=False)
+data_na = mbs.sample_NA_screening(data)
+display(data_na)
